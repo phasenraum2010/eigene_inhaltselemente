cd8b0bf5a333448d0956fe55842faeb9a84bd424
phasenraum2010 <thomas@woehlke.org>
2016-02-24 10:14:38 +0100

    letzte Änderungen 24.02.16

--- Resources/Public/JS/meine_js_datei.js
+++ Resources/Public/JS/meine_js_datei.js
@@ -2,26 +2,23 @@
  Eigene Inhaltselemente
  */
 var $carousel = $('#carousel-id');
-$carousel.carousel();
+$carousel.carousel(
+    interval: 10000;
+);
 var handled=false;
 var itemclicked="";
 $carousel.bind('slide.bs.carousel', function (e) {
     var current=$(e.target).find('.item.active');
     var indx=$(current).index();
-    if((indx+2)>$('.carousel-indicators li').length)
-        indx=-1
-
-    if(!handled)
-    {
-        $('.carousel-indicators li').removeClass('active')
-        $('.carousel-indicators li:nth-child('+(indx+2)+')').addClass('active');
-
+    if((indx+2)>$('.carousel-indicators li').length) {
+        indx = -1;
     }
-    else
-    {
+    if(!handled){
+        $('.carousel-indicators li').removeClass('active');
+        $('.carousel-indicators li:nth-child('+(indx+2)+')').addClass('active');
+    } else {
         handled=!handled;
     }
-
 });
 
 $(".carousel-indicators li").on('click',function(){
